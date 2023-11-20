2fb9478628111c66c951f401cca02002e8cb010b
thatboyrohit <thatboyrohit@gmail.com>
2023-11-20 19:26:50 +0530

    initial commit

---
+++ lib/commons/widgets/custom_button.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed:onTap ,
+     child: Text(text),
+     style: ElevatedButton.styleFrom(
+      minimumSize: const Size(double.infinity, 50)
+     ),
+     );
+  }
+}
